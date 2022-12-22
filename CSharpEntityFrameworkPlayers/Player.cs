82666d927b9ac9a6555fde73fd10728814043ef7
GioPa97 <giovannipagliara97@.com>
2022-12-22 15:43:09 +0100

    Creato Player

---
+++ CSharpEntityFrameworkPlayers/Player.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CSharpEntityFrameworkPlayers
+{
+    [Table("player")]
+    [Index(nameof(PlayerID), IsUnique = true)]
+    public class Player
+    {
+        [Required]
+        public int PlayerID { get; set; }
+
+        [Required]
+        public int MatchAttended { get; set; }
+
+        public int MatchWon { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+
+
+
+    }
+}
